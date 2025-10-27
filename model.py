7044e957c088d68741a45d41d513d36fe707fccf
Soni-japan <soni.mbox@gmail.com>
2025-10-27 22:52:15 +0900

    wip

--- model.py
+++ model.py
@@ -1,9 +1,5 @@
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from dataset import RosbagDataset, load_rosbag_data
-from torch.utils.data import DataLoader
 
 # シードを固定
 torch.manual_seed(42)
@@ -48,6 +44,11 @@ class DNN(nn.Module):
 
 
 if __name__ == "__main__":
+    import torch.optim as optim
+    import matplotlib.pyplot as plt
+    from torch.utils.data import DataLoader
+    from dataset import RosbagDataset, load_rosbag_data
+    
     # デバイスの設定
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
